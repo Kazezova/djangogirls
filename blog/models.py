815bc881e71ec5dac7f8b063532b45ebe4a59555
Kazezova <anar.kazezova@gmail.com>
2020-10-29 15:30:44 +0500

    first

---
+++ blog/models.py
@@ -0,0 +1,21 @@
+# Create your models here.
+from django.conf import settings
+from django.db import models
+from django.utils import timezone
+
+class Post(models.Model): 
+    """
+    Django model Post(saved in the database)
+    """
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE) #link to another model
+    title = models.CharField(max_length=200) #text with limited number of characters
+    text = models.TextField() #text without limit
+    created_date = models.DateTimeField(default=timezone.now) 
+    published_date = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
